b2d1df87cfc0cee9548c0fde0ac4d075bcf7368e
dhung1 <davidhung@email.arizona.edu>
2017-04-28 00:46:24 -0700

    Added ROS integration

--- classify_images.py
+++ classify_images.py
@@ -20,113 +20,115 @@ import tensorflow as tf
 
 import convnets.wideresnet.wideresnet_model as model
 
-# TODO: Import image classifiers!
-
 # Constants
+DEBUG = True
 IMAGE_SIZE = 32
 IMAGE_CHANNELS = 3
 
-parser = argparse.ArgumentParser(
-					description='This program is to be run on the ground station '
-								'side of the 2016-17 computer vision system. It'
-								'continuously scans a directory for images and passes'
-								'them to image classifier(s). Results are sent to the'
-								'Interop Server')
-parser.add_argument('-u', '--userid', default='azautonomous',
-						help='User ID for Interop Server.')
-parser.add_argument('-f', '--format', default='jpg', 
-						help='Input image format. Suggested formats are jpg or png')
-parser.add_argument('-d', '--dir', 
-						help='Directory to scan for images. If no directory provided, '
-								'scans current working directory')
-parser.add_argument('-c', '--checkpoint_dir', default='checkpoints', 
-						help='Path to checkpoint directories. '
-							'Each classifier should be kept in a separate directory '
-							'according to their name (e.g. scope). For example:\n'
-							'\tcheckpoints/\n\t\tshape/\n\t\talphanum/\n\t\tetc...')
-
-args = parser.parse_args()
-
-# TODO: Look Up Tables for converting class indices to human readable strings
 class TargetClassifier():
-	def __init__(self):
+	def __init__(self, userid, checkpoint_dir):
 		# Store Look Up Tables
-		self.shapes = {}
-		self.alphanums = {}
-		self.colors = {}
-		
+		self.shapes = {0 : 'n/a', 1 : 'circle', 2 : 'cross', 3 : 'heptagon', 4 : 'hexagon', 5 : 'octagon', 6 : 'pentagon', 7 : 'quartercircle', 8 : 'rectangle', 9 : 'semicircle', 10 : 'square', 11 : 'star', 12 : 'trapezoid', 13 : 'triangle'}
+		self.alphanums = {0 : 'n/a',  1 : 'A',  2 : 'B',  3 : 'C',  4 : 'D',  5 : 'E',  6 : 'F',  7 : 'G',  8 : 'H',  9 : 'I',  10 : 'J', 11 : 'K', 12 : 'L', 13 : 'M', 14 : 'N', 15 : 'O', 16 : 'P', 17 : 'Q', 18 : 'R', 19 : 'S', 20 : 'T', 21 : 'U', 22 : 'V', 23 : 'W', 24 : 'X', 25 : 'Y', 26 : 'Z', 27 : '0', 28 : '1', 29 : '2', 30 : '3', 31 : '4', 32 : '5', 33 : '6', 34 : '7', 35 : '8', 36 : '9'}
+		self.colors = {0 : 'n/a', 1 : 'white', 2 : 'black', 3 : 'gray', 4 : 'red', 5 : 'blue', 6 : 'green', 7 : 'yellow', 8 : 'purple', 9 : 'brown', 10 : 'orange'}
+		self.orientation = {0 : 'n/a', 1 : 'N', 2 : 'NE', 3 : 'E', 4 : 'SE', 5 : 'S', 6 : 'SW', 7 : 'W', 8 : 'NW'}
+
+		# Store userid
+		self.userid = userid
+
 		# IMPORTANT! Put updated mean standard values here (TODO)
-		self.mean = np.array([0, 0, 0]) # R, G, B
-		self.stddev = np.array([1, 1, 1]) # R, G, B
+		self.mean = np.array([62.026, 128.710, 131.434]) # R, G, B
+		self.stddev = np.array([53.492, 50.432, 52.819]) # R, G, B
 		
 		# Counters/trackers for interop
 		self.target_id = 0
 
 		# Build TensorFlow graphs
-		# TODO: Savers
-		checkpoint_dir = args.checkpoint_dir
 		assert os.path.isdir(checkpoint_dir)
-		# We will need these for the savers later
-		variable_averages = tf.train.ExponentialMovingAverage(
-								wideresnet.MOVING_AVERAGE_DECAY)
-		variables_to_restore = variable_averages.variables_to_restore()
-		saver = tf.train.Saver(variables_to_restore)
-		
-		# Shape
-		self.shape_sess = tf.Session()
-		shape_saver = tf.train.Saver()
-		shape_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'shape'))
-		if shape_ckpt and shape_ckpt.model_checkpoint_path:
-			print('Reading shape model parameters from %s' % shape_ckpt.model_checkpoint_path)
-			#shape_saver.restore(self.shape_sess, self.shape_ckpt.model_checkpoint_path)
-			saver.restore(self.shape_sess, shape_ckpt.model_checkpoint_path)
-		else:
-			print('Error restoring parameters for shape. Ensure checkpoint is stored in ${checkpoint_dir}/shape/')
-			sys.exit(1)
-		self.inputs_shape = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
-		self.logits_shape = model.inference(inputs_shape, 14, scope='shape') # 13 shapes + background
-		
-		# Shape Color
-		self.shape_color_sess = tf.Session()
-		shape_color_saver = tf.train.Saver()
-		shape_color_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'shape_color'))
-		if shape_color_ckpt and shape_color_ckpt.model_checkpoint_path:
-			print('Reading shape_color model parameters from %s' % shape_color_ckpt.model_checkpoint_path)
-			#shape_color_saver.restore(self.shape_color_sess , shape_color_ckpt.model_checkpoint_path)
-			saver.restore(self.shape_color_sess , shape_color_ckpt.model_checkpoint_path)
-		else:
-			print('Error restoring parameters for shape_color. Ensure checkpoint is stored in ${checkpoint_dir}/shape_color/')
-			sys.exit(1)
-		self.inputs_shape_color = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
-		self.logits_shape_color = model.inference(inputs_shape_color, 11, scope='shape_color') # 10 colors + background
-		
-		# Alphanum
-		self.alphanum_sess = tf.Session()
-		alphanum_saver = tf.train.Saver()
-		alphanum_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'alphanum'))
-		if alphanum_ckpt and alphanum_ckpt.model_checkpoint_path:
-			print('Reading alphanum model parameters from %s' % alphanum_ckpt.model_checkpoint_path)
-			#alphanum_saver.restore(self.alphanum_sess, alphanum_ckpt.model_checkpoint_path)
-			saver.restore(self.alphanum_sess, alphanum_ckpt.model_checkpoint_path)
-		else:
-			print('Error restoring parameters for alphanum. Ensure checkpoint is stored in ${checkpoint_dir}/alphanum/')
-			sys.exit(1)
-		self.inputs_alphanum = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
-		self.logits_alphanum = model.inference(inputs_alphanum, 37, scope='alphanum') # 36 alphanums + background
-		
-		# Alphanum Color
-		self.alphanum_color_sess = tf.Session()
-		alphanum_color_saver = tf.train.Saver()
-		alphanum_color_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'alphanum_color'))
-		if alphanum_color_ckpt and alphanum_color_ckpt.model_checkpoint_path:
-			print('Reading alphanum_color model parameters from %s' % alphanum_color_ckpt.model_checkpoint_path)
-			#alphanum_color_saver.restore(self.alphanum_color_sess, alphanum_color_ckpt.model_checkpoint_path)
-			saver.restore(self.alphanum_color_sess, alphanum_color_ckpt.model_checkpoint_path)
-		else:
-			print('Error restoring parameters for alphanum_color. Ensure checkpoint is stored in ${checkpoint_dir}/alphanum_color/')
-			sys.exit(1)
-		self.inputs_alphanum_color = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
-		self.logits_alphanum_color = model.inference(inputs_alphanum_color, 11, scope='alphanum_color') # 10 colors + background
+		# Shape graph
+		self.shape_graph = tf.Graph()
+		with self.shape_graph.as_default():
+			self.inputs_shape = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
+			self.logits_shape = model.inference(self.inputs_shape, 14, scope='shapes') # 13 shapes + background
+			variable_averages = tf.train.ExponentialMovingAverage(
+									model.MOVING_AVERAGE_DECAY)
+			variables_to_restore = variable_averages.variables_to_restore()
+			saver = tf.train.Saver(variables_to_restore)
+			
+			self.shape_sess = tf.Session() # graph=self.shape_graph
+			#shape_saver = tf.train.Saver()
+			shape_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'shape'))
+			if shape_ckpt and shape_ckpt.model_checkpoint_path:
+				print('Reading shape model parameters from %s' % shape_ckpt.model_checkpoint_path)
+				#shape_saver.restore(self.shape_sess, self.shape_ckpt.model_checkpoint_path)
+				saver.restore(self.shape_sess, shape_ckpt.model_checkpoint_path)
+			else:
+				print('Error restoring parameters for shape. Ensure checkpoint is stored in ${checkpoint_dir}/shape/')
+				# sys.exit(1)
+	
+		# Shape color graph
+		self.shape_color_graph = tf.Graph()
+		with self.shape_color_graph.as_default():
+			self.inputs_shape_color = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
+			self.logits_shape_color = model.inference(self.inputs_shape_color, 11, scope='shape_color') # 10 shape_colors + background
+			variable_averages = tf.train.ExponentialMovingAverage(
+									model.MOVING_AVERAGE_DECAY)
+			variables_to_restore = variable_averages.variables_to_restore()
+			saver = tf.train.Saver(variables_to_restore)
+			
+			self.shape_color_sess = tf.Session() # graph=self.shape_color_graph
+			#shape_color_saver = tf.train.Saver()
+			shape_color_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'shape_color'))
+			if shape_color_ckpt and shape_color_ckpt.model_checkpoint_path:
+				print('Reading shape_color model parameters from %s' % shape_color_ckpt.model_checkpoint_path)
+				#shape_color_saver.restore(self.shape_color_sess, self.shape_color_ckpt.model_checkpoint_path)
+				saver.restore(self.shape_color_sess, shape_color_ckpt.model_checkpoint_path)
+			else:
+				print('Error restoring parameters for shape_color. Ensure checkpoint is stored in ${checkpoint_dir}/shape_color/')
+				print self.shape_color_graph	
+				# sys.exit(1)
+	
+		# Alphanum graph
+		self.alphanum_graph = tf.Graph()
+		with self.alphanum_graph.as_default():
+			self.inputs_alphanum = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
+			self.logits_alphanum = model.inference(self.inputs_alphanum, 37, scope='alphanum') # 37 alphanums + background
+			variable_averages = tf.train.ExponentialMovingAverage(
+									model.MOVING_AVERAGE_DECAY)
+			variables_to_restore = variable_averages.variables_to_restore()
+			saver = tf.train.Saver(variables_to_restore)
+			
+			self.alphanum_sess = tf.Session()
+			#alphanum_saver = tf.train.Saver()
+			alphanum_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'alphanum'))
+			if alphanum_ckpt and alphanum_ckpt.model_checkpoint_path:
+				print('Reading alphanum model parameters from %s' % alphanum_ckpt.model_checkpoint_path)
+				#alphanum_saver.restore(self.alphanum_sess, self.alphanum_ckpt.model_checkpoint_path)
+				saver.restore(self.alphanum_sess, alphanum_ckpt.model_checkpoint_path)
+			else:
+				print('Error restoring parameters for alphanum. Ensure checkpoint is stored in ${checkpoint_dir}/alphanum/')
+				# sys.exit(1)
+	
+		# Alphanum color graph
+		self.alphanum_color_graph = tf.Graph()
+		with self.alphanum_color_graph.as_default():
+			self.inputs_alphanum_color = tf.placeholder(tf.float32, shape=[1, IMAGE_SIZE, IMAGE_SIZE, IMAGE_CHANNELS])
+			self.logits_alphanum_color = model.inference(self.inputs_alphanum_color, 11, scope='alphanum_color') # 10 alphanum_colors + background
+			variable_averages = tf.train.ExponentialMovingAverage(
+									model.MOVING_AVERAGE_DECAY)
+			variables_to_restore = variable_averages.variables_to_restore()
+			saver = tf.train.Saver(variables_to_restore)
+			
+			self.alphanum_color_sess = tf.Session()
+			#alphanum_color_saver = tf.train.Saver()
+			alphanum_color_ckpt = tf.train.get_checkpoint_state(os.path.join(checkpoint_dir, 'alphanum_color'))
+			if alphanum_color_ckpt and alphanum_color_ckpt.model_checkpoint_path:
+				print('Reading alphanum_color model parameters from %s' % alphanum_color_ckpt.model_checkpoint_path)
+				#alphanum_color_saver.restore(self.alphanum_color_sess, self.alphanum_color_ckpt.model_checkpoint_path)
+				saver.restore(self.alphanum_color_sess, alphanum_color_ckpt.model_checkpoint_path)
+			else:
+				print('Error restoring parameters for alphanum_color. Ensure checkpoint is stored in ${checkpoint_dir}/alphanum_color/')
+				# sys.exit(1)
 
 	def preprocess_image(self, image):
 		''' Preprocess image for classification
@@ -139,10 +141,10 @@ class TargetClassifier():
 		# Resize image as necessary
 		if (np.greater(im.shape[:2], [IMAGE_SIZE, IMAGE_SIZE]).any()):
 			# Scale down
-			im = cv2.resize(im, dsize=(IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.CV_INTER_AREA)
+			im = cv2.resize(im, dsize=(IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_AREA)
 		elif (np.less(im.shape[:2], [IMAGE_SIZE, IMAGE_SIZE]).any()):
 			# Scale up
-			im = cv2.resize(im, dsize=(IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.CV_INTER_CUBIC)
+			im = cv2.resize(im, dsize=(IMAGE_SIZE, IMAGE_SIZE), interpolation=cv2.INTER_CUBIC)
 		# MeanStd normalization
 		im = np.subtract(im, self.mean)
 		im = np.divide(im, self.stddev)
@@ -157,6 +159,7 @@ class TargetClassifier():
 		'''
 		predictions = self.shape_sess.run([self.logits_shape],
 												feed_dict={self.inputs_shape: image})
+		print predictions # TODO DELETEME
 		class_out = np.argmax(predictions)
 		confidence = np.max(predictions)
 		# TODO: Do something with the confidence
@@ -169,7 +172,7 @@ class TargetClassifier():
 					str: The classified color, in human readable text
 		'''
 		predictions = self.shape_color_sess.run([self.logits_shape_color],
-												feed_dict={self.inputs_shape_color: input_image})
+												feed_dict={self.inputs_shape_color: image})
 		class_out = np.argmax(predictions)
 		confidence = np.max(predictions)
 		# TODO: Do something with the confidence
@@ -189,7 +192,7 @@ class TargetClassifier():
 		while (rot < 360):
 			# TODO: Rotate image clockwise by rot degrees
 			predictions = self.alphanum_sess.run([self.logits_alphanum],
-													feed_dict={self.inputs_alphanum: inputs_alphanum})
+													feed_dict={self.inputs_alphanum: image})
 			class_out_dict[np.max(predictions)] = np.argmax(predictions)
 			rot += 45 # 45 degree stride. If computation budget allows, consider increasing to 22.5 deg
 		confidence = max(class_out_dict) # Maximum confidence from classifications
@@ -204,7 +207,7 @@ class TargetClassifier():
 					str: The classified color, in human readable text
 		'''
 		predictions = self.alphanum_color_sess.run([self.logits_alphanum_color],
-													feed_dict={self.inputs_alphanum_color: input_image})
+													feed_dict={self.inputs_alphanum_color: image})
 		class_out = np.argmax(predictions)
 		confidence = np.max(predictions)
 		# TODO: Do something with the confidence
@@ -220,7 +223,7 @@ class TargetClassifier():
 		'''
 		return 0, 0 # FIXME/TODO
 	
-	def check_valid(packet):
+	def check_valid(self, packet):
 		''' Check whether the prepared output packet is valid
 				Args:
 					dict: dictionary (JSON) of proposed output packet
@@ -238,7 +241,7 @@ class TargetClassifier():
 	
 			return True
 	
-	def classify_and_maybe_transmit(image, location=None, orientation=None):
+	def classify_and_maybe_transmit(self, image, location=None, orientation=None):
 		''' Main worker function for image classification. Transmits depending on validity
 			Args:
 				image: np.array of size [width, height, depth]
@@ -250,14 +253,27 @@ class TargetClassifier():
 		
 		# Run respective image classifiers
 		shape = self.classify_shape(image)
-		background_color = self.classify_shape_color(image)
-		alphanumeric, rot = self.classify_letter(image)
-		alphanumeric_color = self.classify_letter_color(image)
-		latitude, longitude = self.extract_location(image) # TODO - input arg?
+		#background_color = self.classify_shape_color(image)
+		#alphanumeric, rot = self.classify_letter(image)
+		#alphanumeric_color = self.classify_letter_color(image)
+		#latitude, longitude = self.extract_location(image) # TODO - input arg?
 		# TODO: Get orientation using orientation_in + rot
+
+		# TODO: Delete these placeholders
+		background_color = 'LOL'
+		alphanumeric, rot = 'LOL', 1337
+		alphanumeric_color = 'REKT'
+		latitude, longitude = 1337, 9001
+
+		if DEBUG:
+			print 'Shape =', shape
+			print 'Shape Color =', background_color
+			print 'Alphanumeric =', alphanumeric
+			print 'Alphanum Color =', alphanumeric_color
+			print 'Lat, Lon =', latitude, ',', longitude
 	
 		packet = {
-				"user": args.userid, # TODO: What will our user id be?
+				"user": self.userid, # TODO: What will our user id be?
 				"type": "standard",
 				"latitude": latitude,
 				"longitude": longitude,
@@ -271,8 +287,8 @@ class TargetClassifier():
 				}
 	
 		# Check for false positives or otherwise invalid targets
-		if check_valid(packet):
-			print('INFO: Transmitting target found in {}'.format(f))
+		if self.check_valid(packet):
+			print('INFO: Transmitting target %d' % self.target_id)
 			packet["id"] = self.target_id
 			json_packet = json.dumps(packet)
 			# TODO: Transmit data to interop server
@@ -282,6 +298,28 @@ class TargetClassifier():
 			print('INFO: Processed invalid target in {}'.format(f))
 
 def main():
+	# Create command line args
+	parser = argparse.ArgumentParser(
+						description='This program is to be run on the ground station '
+									'side of the 2016-17 computer vision system. It'
+									'continuously scans a directory for images and passes'
+									'them to image classifier(s). Results are sent to the'
+									'Interop Server')
+	parser.add_argument('-u', '--userid', default='azautonomous',
+							help='User ID for Interop Server.')
+	parser.add_argument('-f', '--format', default='jpg', 
+							help='Input image format. Suggested formats are jpg or png')
+	parser.add_argument('-d', '--dir', 
+							help='Directory to scan for images. If no directory provided, '
+									'scans current working directory')
+	parser.add_argument('-c', '--checkpoint_dir', required=True, 
+								help='Path to checkpoint directories. '
+								'Each classifier should be kept in a separate directory '
+								'according to their name (e.g. scope). For example, '
+								'checkpoints/ with subdirectories shape/, alphanum/, etc')
+	
+	args = parser.parse_args()
+
 	# Process command line args
 	if args.dir is not None:
 		 directory = args.dir
@@ -293,7 +331,7 @@ def main():
 	assert os.path.exists(directory)
 
 	# Initialize classifiers
-	classifier = TargetClassifier()
+	classifier = TargetClassifier(args.userid, args.checkpoint_dir)
 
 	print 'Running on directory:\t\t', directory
 	print 'Searching for images of format:\t', ext
---
+++ classify_ros.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+import argparse
+
+from obe_toolset.msg import ImageAndPose
+from cv_bridge import CvBridge
+from classify_images import TargetClassifier
+
+# Create command line args
+parser = argparse.ArgumentParser(
+					description='This program is to be run on the ground station '
+								'side of the 2016-17 computer vision system. It'
+								'continuously scans a directory for images and passes'
+								'them to image classifier(s). Results are sent to the'
+								'Interop Server')
+parser.add_argument('-u', '--userid', default='azautonomous',
+						help='User ID for Interop Server.')
+parser.add_argument('-c', '--checkpoint_dir', default='checkpoints', 
+							help='Path to checkpoint directories. '
+							'Each classifier should be kept in a separate directory '
+							'according to their name (e.g. scope). For example, '
+							'checkpoints/ with subdirectories shape/, alphanum/, etc')
+
+args = parser.parse_args()
+
+classifier = TargetClassifier(args.userid, args.checkpoint_dir)
+bridge = CvBridge()
+
+def callback(data):
+	image = bridge.imgmsg_to_cv2(data.image)
+	# TODO: Parse these
+	data.x # UTM coordinates in meters, east/west
+	data.y # UTM coordinates in meters, north/south
+	data.z # AGL altitude in meters
+	data.roll # Unused
+	data.pitch # Unused
+	data.yaw # Radians CCW from east
+	classifier.classify_and_maybe_transmit(image)
+
+def run():
+	rospy.init_node('ROI_Listener', anonymous=True)
+	rospy.Subscriber('ROIs', ImageAndPose, callback)
+
+	rospy.spin()
+
+if __name__ == '__main__':
+	run()
